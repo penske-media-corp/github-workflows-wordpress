b1ae2e45c77877ab40b69ac4d4a0a0be68c93793
Hau Vong <hvong@pmc.com>
2025-02-06 08:59:20 -0800

    PR feeback

--- composites/enforce-pmc-scripts/enforce-pmc-scripts.sh
+++ composites/enforce-pmc-scripts/enforce-pmc-scripts.sh
@@ -10,20 +10,20 @@ find_package_json() {
     if jq -e '.dependencies["@wordpress/scripts"] // .devDependencies["@wordpress/scripts"] // .dependencies["@penskemediacorp/wordpress-scripts"] // .devDependencies["@penskemediacorp/wordpress-scripts"]' "$1/package.json" >/dev/null 2>&1; then
       echo "$1/package.json"
     fi
-  elif [[ ${1} != "." && $1 != $2 ]]; then
-    find_package_json $(dirname $1) $2
+  elif [[ "${1}" != "." && "$1" != "$2" ]]; then
+    find_package_json $(dirname "$1") "$2"
   fi
 }
 
 GIT_REPO=$(git remote get-url origin)
 if [[ -z ${DEFAULT_BRANCH} ]]; then
   DEFAULT_BRANCH=$(
-    git ls-remote --symref ${GIT_REPO} HEAD 2>stderr \
+    git ls-remote --symref "${GIT_REPO}" HEAD 2>stderr \
       | awk '/^ref:/ {sub(/refs\/heads\//, "", $2); print $2}'
     )
 fi
 
-git fetch origin ${DEFAULT_BRANCH}
+git fetch origin ${DEFAULT_BRANCH} >/dev/null 2>&1
 
 if [[ -f .pmc-scripts ]]; then
   source .pmc-scripts
@@ -31,7 +31,7 @@ fi
 
 # Enforce TypeScript for new files
 for file in $(
-  if [[ -z ${JS_IGNORE_PATTERNS} ]]; then
+  if [[ -z "${JS_IGNORE_PATTERNS}" ]]; then
     git --no-pager diff --diff-filter=A "origin/${DEFAULT_BRANCH}" --name-only \
       -- '*.js' '*.ts' '*.jsx' '*.tsx' ':!/vendor/' ':!/.cache/' ':!/node_modules/' \
       | grep -E '\.jsx?$'
@@ -42,24 +42,24 @@ for file in $(
       | grep -v -E "${JS_IGNORE_PATTERNS}"
   fi
 ); do
-  path=$(dirname $file)
+  path=$(dirname "$file")
   if [[ "${GIT_REPO}" =~ "penske-media-corp/pmc-plugins" ]]; then
     package_json=$( find_package_json "${path}" $(echo "${path}" | cut -d/ -f1) )
   else
     package_json=$( find_package_json "${path}" )
   fi
 
-  if [[ -n $package_json ]]; then
-    NEW_JS_FILES+=(${file})
-    if [[ -z $( grep "@penskemediacorp/wordpress-scripts" $package_json ) || -z $( grep "pmc-scripts" $package_json ) ]]; then
-      NEED_UPDATE_PACKAGE_JSONS+=($package_json)
+  if [[ -n "$package_json" ]]; then
+    NEW_JS_FILES+=("${file}")
+    if [[ -z $( grep "@penskemediacorp/wordpress-scripts" "$package_json" ) || -z $( grep "pmc-scripts" "$package_json" ) ]]; then
+      NEED_UPDATE_PACKAGE_JSONS+=("$package_json")
     fi
   fi
 done
 
 # Enforce package.json to use @penskemediacorp/wordpress-scripts package
 for package_json in $(
-  if [[ -z ${JS_IGNORE_PATTERNS} ]]; then
+  if [[ -z "${JS_IGNORE_PATTERNS}" ]]; then
     git --no-pager diff --diff-filter=A "origin/${DEFAULT_BRANCH}" --name-only \
       -- '*.json' ':!/vendor/' ':!/.cache/' ':!/node_modules/'
   else
@@ -68,23 +68,23 @@ for package_json in $(
       | grep -v -E "${JS_IGNORE_PATTERNS}"
   fi
 ); do
-  if [[ -z $( grep "@penskemediacorp/wordpress-scripts" $package_json ) || -z $( grep "pmc-scripts" $package_json ) ]]; then
-    NEED_UPDATE_PACKAGE_JSONS+=($package_json)
+  if [[ -z $( grep "@penskemediacorp/wordpress-scripts" "$package_json" ) || -z $( grep "pmc-scripts" "$package_json" ) ]]; then
+    NEED_UPDATE_PACKAGE_JSONS+=("$package_json")
   fi
 done
 
-if [[ -n ${NEW_JS_FILES[@]} ]]; then
+if [[ -n "${NEW_JS_FILES[@]}" ]]; then
   HAS_ERROR=true
   echo -e "\nTypeScript is required for the following JS file(s): "
-  printf ' - %s\n' ${NEW_JS_FILES[@]} | sort -u
+  printf ' - %s\n' "${NEW_JS_FILES[@]}" | sort -u
 fi
-if [[ -n ${NEED_UPDATE_PACKAGE_JSONS[@]} ]]; then
+if [[ -n "${NEED_UPDATE_PACKAGE_JSONS[@]}" ]]; then
   HAS_ERROR=true
   echo -e "\nBuild script needs to use @penskemediacorp/wordpress-scripts package:"
-  printf ' - %s\n' ${NEED_UPDATE_PACKAGE_JSONS[@]} | sort -u
+  printf ' - %s\n' "${NEED_UPDATE_PACKAGE_JSONS[@]}" | sort -u
 fi
 
-if [[ -n ${HAS_ERROR} ]]; then
+if [[ -n "${HAS_ERROR}" ]]; then
   echo -e "\nErrors detected.\n"
   exit 1
 fi
